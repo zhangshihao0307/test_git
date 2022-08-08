581c176ad5506a9a046007ba613b412bdb847837
zhangshihao <2813446928@qq.com>
2022-08-08 21:14:16 +0800

    second commit_test

--- src/main/java/zhangshihao/git/GItTest.java
+++ src/main/java/zhangshihao/git/GItTest.java
@@ -3,5 +3,6 @@ package zhangshihao.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
