e5d5ddbd56236c18a08d451c01a332c575892403
zhangshihao <2813446928@qq.com>
2022-08-08 21:14:46 +0800

    third commit_test

--- src/main/java/zhangshihao/git/GItTest.java
+++ src/main/java/zhangshihao/git/GItTest.java
@@ -4,5 +4,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
